89171840aa152e311b5be7c276402ffda0574f87
Timo Goebel <mail@timogoebel.name>
2017-08-23 21:32:43 +0200

    add ui notification

---
+++ app/services/foreman_expire_hosts/ui_notifications/hosts/base.rb
@@ -0,0 +1,68 @@
+module ForemanExpireHosts
+  module UINotifications
+    module Hosts
+      class Base < ::UINotifications::Hosts::Base
+        private
+
+        def create
+          return add_notification unless find_notification
+          redeliver! if redeliver?
+          find_notification
+        end
+
+        def add_notification
+          ::Notification.create!(
+            initiator: initiator,
+            subject: subject,
+            message: parsed_message,
+            audience: audience,
+            notification_blueprint: blueprint
+          )
+        end
+
+        def message
+          blueprint.message
+        end
+
+        def parsed_message
+          ::UINotifications::StringParser.new(
+            message,
+            message_variables
+          ).to_s
+        end
+
+        def message_variables
+          {
+            subject: subject,
+            initator: initiator
+          }
+        end
+
+        def update_notification
+          find_notification
+            .update_attributes(expired_at: blueprint.expired_at, :message => parsed_message)
+        end
+
+        def redeliver!
+          recipients = find_notification.notification_recipients
+          recipients.update_all(seen: false) # rubocop:disable Rails/SkipsModelValidations
+          recipients.pluck(:user_id).each do |user_id|
+            ::UINotifications::CacheHandler.new(user_id).clear
+          end
+        end
+
+        def redeliver?
+          false
+        end
+
+        def find_notification
+          blueprint.notifications.find_by(subject: subject)
+        end
+
+        def blueprint
+          @blueprint ||= NotificationBlueprint.find_by(name: blueprint_name)
+        end
+      end
+    end
+  end
+end
---
+++ app/services/foreman_expire_hosts/ui_notifications/hosts/expiry_warning.rb
@@ -0,0 +1,28 @@
+module ForemanExpireHosts
+  module UINotifications
+    module Hosts
+      class ExpiryWarning < Base
+        include ActionView::Helpers::DateHelper
+
+        def blueprint_name
+          'expire_hosts_expiry_warning'
+        end
+
+        private
+
+        # Nag the user about expiring hosts
+        def redeliver?
+          true
+        end
+
+        def message
+          N_('%{subject} will expire in %{relative_expiry_time}.')
+        end
+
+        def message_variables
+          super.merge(:relative_expiry_time => time_ago_in_words(subject.expired_on))
+        end
+      end
+    end
+  end
+end
