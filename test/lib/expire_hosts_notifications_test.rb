7cd27dd401ce908b960d36ee64097eaf1214c632
Timo Goebel <mail@timogoebel.name>
2019-04-05 13:15:54 +0200

    do not send empty mail to additional recipients

--- test/lib/expire_hosts_notifications_test.rb
+++ test/lib/expire_hosts_notifications_test.rb
@@ -61,16 +61,21 @@ class ExpireHostsNotificationsTest < ActiveSupport::TestCase
     context 'with additional recipients' do
       setup do
         Setting[:host_expiry_email_recipients] = 'test@example.com, test2.example.com'
-        FactoryBot.create_list(:host, 2, :expired, :owner => user)
       end
 
       test 'should deliver notification to additional recipients' do
+        FactoryBot.create_list(:host, 2, :expired, :owner => user)
         ExpireHostsNotifications.delete_expired_hosts
-        assert_equal 3, ActionMailer::Base.deliveries.count
+        assert_equal 2, ActionMailer::Base.deliveries.count
         assert_includes ActionMailer::Base.deliveries.first.subject, 'Deleted expired hosts'
         assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user.mail
         assert_includes ActionMailer::Base.deliveries.flat_map(&:to), 'test@example.com'
         assert_includes ActionMailer::Base.deliveries.flat_map(&:to), 'test2.example.com'
+      end
+
+      test 'should not deliver a mail notification for empty host list' do
+        ExpireHostsNotifications.delete_expired_hosts
+        assert_equal 0, ActionMailer::Base.deliveries.count
       end
     end
   end
@@ -114,59 +119,75 @@ class ExpireHostsNotificationsTest < ActiveSupport::TestCase
 
     setup do
       Setting['notify1_days_before_host_expiry'] = 7
-      hosts
     end
 
-    test 'should send a ui notification per host' do
-      assert_difference('blueprint.notifications.count', 2) do
+    context 'with expired hosts' do
+      setup do
+        hosts
+      end
+
+      test 'should send a ui notification per host' do
+        assert_difference('blueprint.notifications.count', 2) do
+          ExpireHostsNotifications.deliver_expiry_warning_notification
+        end
+        hosts.each do |host|
+          notification = Notification.find_by(
+            notification_blueprint_id: blueprint.id,
+            subject_id: host.id,
+            subject_type: 'Host::Base'
+          )
+          assert_equal 1, notification.notification_recipients.where(user_id: user.id).count
+        end
+      end
+
+      test 'should redisplay read ui notification' do
         ExpireHostsNotifications.deliver_expiry_warning_notification
+        notification = Notification.find_by(notification_blueprint_id: blueprint.id, subject_id: hosts.first.id)
+        assert_not_nil notification
+        assert_equal 1, NotificationRecipient.where(notification_id: notification.id).update_all(seen: true) # rubocop:disable Rails/SkipsModelValidations
+        ExpireHostsNotifications.deliver_expiry_warning_notification
+        assert_equal 1, NotificationRecipient.where(notification_id: notification.id, seen: false).count
       end
-      hosts.each do |host|
-        notification = Notification.find_by(
-          notification_blueprint_id: blueprint.id,
-          subject_id: host.id,
-          subject_type: 'Host::Base'
-        )
-        assert_equal 1, notification.notification_recipients.where(user_id: user.id).count
+
+      test 'should send a single notification' do
+        ExpireHostsNotifications.deliver_expiry_warning_notification
+        assert_equal 1, ActionMailer::Base.deliveries.count
+        assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
+      end
+
+      test 'should send two notifications for two users' do
+        owner2 = FactoryBot.create(:user, :with_mail)
+        FactoryBot.create(:host, :expires_in_a_week, :owner => owner2)
+        ExpireHostsNotifications.deliver_expiry_warning_notification
+        assert_equal 2, ActionMailer::Base.deliveries.count
+        assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
+        assert_includes ActionMailer::Base.deliveries.last.subject, 'Expiring hosts in foreman'
+      end
+
+      test 'should send three notifications for three users' do
+        user2 = FactoryBot.create(:user, :with_mail)
+        user3 = FactoryBot.create(:user, :with_mail, :usergroups => [usergroup])
+        FactoryBot.create(:host, :expires_in_a_week, :owner => user2)
+        FactoryBot.create(:host, :expires_in_a_week, :owner => usergroup)
+        ExpireHostsNotifications.deliver_expiry_warning_notification
+        assert_equal 3, ActionMailer::Base.deliveries.count
+        assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
+        assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user.mail
+        assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user2.mail
+        assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user3.mail
+        assert_equal 1, ActionMailer::Base.deliveries.flat_map(&:subject).uniq.count
       end
     end
 
-    test 'should redisplay read ui notification' do
-      ExpireHostsNotifications.deliver_expiry_warning_notification
-      notification = Notification.find_by(notification_blueprint_id: blueprint.id, subject_id: hosts.first.id)
-      assert_not_nil notification
-      assert_equal 1, NotificationRecipient.where(notification_id: notification.id).update_all(seen: true) # rubocop:disable Rails/SkipsModelValidations
-      ExpireHostsNotifications.deliver_expiry_warning_notification
-      assert_equal 1, NotificationRecipient.where(notification_id: notification.id, seen: false).count
-    end
+    context 'with additional recipients' do
+      setup do
+        Setting[:host_expiry_email_recipients] = 'test@example.com, test2.example.com'
+      end
 
-    test 'should send a single notification' do
-      ExpireHostsNotifications.deliver_expiry_warning_notification
-      assert_equal 1, ActionMailer::Base.deliveries.count
-      assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
-    end
-
-    test 'should send two notifications for two users' do
-      owner2 = FactoryBot.create(:user, :with_mail)
-      FactoryBot.create(:host, :expires_in_a_week, :owner => owner2)
-      ExpireHostsNotifications.deliver_expiry_warning_notification
-      assert_equal 2, ActionMailer::Base.deliveries.count
-      assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
-      assert_includes ActionMailer::Base.deliveries.last.subject, 'Expiring hosts in foreman'
-    end
-
-    test 'should send three notifications for three users' do
-      user2 = FactoryBot.create(:user, :with_mail)
-      user3 = FactoryBot.create(:user, :with_mail, :usergroups => [usergroup])
-      FactoryBot.create(:host, :expires_in_a_week, :owner => user2)
-      FactoryBot.create(:host, :expires_in_a_week, :owner => usergroup)
-      ExpireHostsNotifications.deliver_expiry_warning_notification
-      assert_equal 3, ActionMailer::Base.deliveries.count
-      assert_includes ActionMailer::Base.deliveries.first.subject, 'Expiring hosts in foreman'
-      assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user.mail
-      assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user2.mail
-      assert_includes ActionMailer::Base.deliveries.flat_map(&:to), user3.mail
-      assert_equal 1, ActionMailer::Base.deliveries.flat_map(&:subject).uniq.count
+      test 'should not send empty reminders' do
+        ExpireHostsNotifications.deliver_expiry_warning_notification
+        assert_equal 0, ActionMailer::Base.deliveries.count
+      end
     end
   end
 end
