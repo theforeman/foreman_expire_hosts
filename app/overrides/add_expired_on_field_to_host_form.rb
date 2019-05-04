a4f6d938c92a4ab7d69d517524ace8c08245dcaf
Michael Moll <kvedulv@kvedulv.de>
2019-05-04 14:53:05 +0200

    fix new rubocop violations

--- app/overrides/add_expired_on_field_to_host_form.rb
+++ app/overrides/add_expired_on_field_to_host_form.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => 'hosts/_form',
-  :name         => 'host_form_expired_on_field',
+  :name => 'host_form_expired_on_field',
   :insert_after => 'div#model_name',
-  :partial      => 'hosts/expired_on_field'
+  :partial => 'hosts/expired_on_field'
 )
--- app/overrides/deleted_expired_host_comment_in_audits.rb
+++ app/overrides/deleted_expired_host_comment_in_audits.rb
@@ -1,13 +1,13 @@
 Deface::Override.new(
-  :virtual_path  => 'audits/_list',
-  :name          => 'deleted_expired_host_audit_comment_in_list',
+  :virtual_path => 'audits/_list',
+  :name => 'deleted_expired_host_audit_comment_in_list',
   :insert_bottom => 'div.row div.audit-content',
-  :text          => "\n <%= destroyed_expired_host_audit_comment_in_list(audit) %>"
+  :text => "\n <%= destroyed_expired_host_audit_comment_in_list(audit) %>"
 )
 
 Deface::Override.new(
-  :virtual_path  => 'audits/show',
-  :name          => 'deleted_expired_host_audit_comment_in_show',
+  :virtual_path => 'audits/show',
+  :name => 'deleted_expired_host_audit_comment_in_show',
   :insert_bottom => 'div#tab1 table',
-  :text          => "\n <%= destroyed_expired_host_audit_comment_in_show(@audit) %>"
+  :text => "\n <%= destroyed_expired_host_audit_comment_in_show(@audit) %>"
 )
