797bdb7481f1a59bff4267e23abc8c01e9b9fb36
Michael Moll <kvedulv@kvedulv.de>
2019-05-04 14:53:05 +0200

    fix new rubocop violations

--- app/overrides/add_expired_on_field_to_host_show.rb
+++ app/overrides/add_expired_on_field_to_host_show.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path  => 'hosts/show',
-  :name          => 'host_expiry_waring_in_show',
+  :virtual_path => 'hosts/show',
+  :name => 'host_expiry_waring_in_show',
   :insert_before => '#host-show',
-  :partial       => 'hosts/expired_message.html.erb'
+  :partial => 'hosts/expired_message.html.erb'
 )
--- app/overrides/add_js_to_host_index.rb
+++ app/overrides/add_js_to_host_index.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path  => 'hosts/_list',
-  :name          => 'host_list_expiration_js',
+  :virtual_path => 'hosts/_list',
+  :name => 'host_list_expiration_js',
   :insert_before => '#confirmation-modal',
-  :text          => "<%= stylesheet 'foreman_expire_hosts/application' %><%= javascript 'foreman_expire_hosts/application' %>"
+  :text => "<%= stylesheet 'foreman_expire_hosts/application' %><%= javascript 'foreman_expire_hosts/application' %>"
 )
