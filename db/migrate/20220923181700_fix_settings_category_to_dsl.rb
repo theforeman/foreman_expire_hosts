6c35f357aa3abb70e48124fe7149331f3aaa6de2
Leos Stejskal <github@stejskalleos.cz>
2023-01-26 14:33:14 +0100

    Migrate category settings only if column exists

--- db/migrate/20220923181700_fix_settings_category_to_dsl.rb
+++ db/migrate/20220923181700_fix_settings_category_to_dsl.rb
@@ -2,6 +2,6 @@
 
 class FixSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
-    Setting.where(category: 'Setting::ExpireHost').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::ExpireHost').update_all(category: 'Setting') if column_exists?(:settings, :category)
   end
 end
