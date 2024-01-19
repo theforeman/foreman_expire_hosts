bde5199d785484e0cec4ce745f2cbbfd247f49cd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-01-19 13:55:59 +0100

    Conditionally include PuppetRelatedHelper

--- test/helpers/hosts_helper_test.rb
+++ test/helpers/hosts_helper_test.rb
@@ -5,7 +5,11 @@ require 'test_plugin_helper'
 class HostsHelperTest < ActionView::TestCase
   include HostsHelper
   include HostDescriptionHelper
-  include PuppetRelatedHelper
+  # Foreman 3.7 dropped this via
+  # https://github.com/theforeman/foreman/commit/6cdb8c7a9ebd790537510213e43537a4a87189d6
+  # Foreman 3.9 bought it back via
+  # https://github.com/theforeman/foreman/commit/37a99c70ebd9e4d6f03af546dfe36e7678e2bcf3
+  include PuppetRelatedHelper if defined?(PuppetRelatedHelper)
   include ForemanExpireHosts::HostsHelper
   include ApplicationHelper
 
